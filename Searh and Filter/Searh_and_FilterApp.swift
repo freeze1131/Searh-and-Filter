9528750ee7bce7428fa293440c672a6e91f733e3
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-07 13:51:57 +0300

    Initial Commit

---
+++ Searh and Filter/Searh_and_FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Searh_and_FilterApp.swift
+//  Searh and Filter
+//
+//  Created by Ahmet Ozen on 7.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Searh_and_FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
